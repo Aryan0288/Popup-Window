e9c14d9cb6d0807335392137f6cd7aeb1b686c17
Aryan0288 <aryansharma31658@gmail.com>
2023-09-15 10:56:40 +0530

    add popUp project

---
+++ unwrap.js
@@ -0,0 +1,19 @@
+
+const p=document.querySelector(".modal");
+const overl=document.querySelector(".overlay");
+// const overl=document.querySelector(".overlay");
+const openModal=() =>{
+    p.classList.add("show");
+    overl.classList.add("overlayactive");
+    // overl.classList.add('overlayactive');
+    console.log('a');
+};
+const closeModal=() =>{
+    p.classList.remove('show');
+    overl.classList.remove('overlayactive');
+    console.log('a');
+};
+
+
+
+
